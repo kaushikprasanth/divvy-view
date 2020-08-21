60a9e42aabf21be7b97b71782902c3b57d88e78f
kaushikprasanth <kaushikprasanth94@yahoo.com>
2020-08-21 01:29:25 -0500

    Skip DB Config

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module AppDemoV2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.0
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
